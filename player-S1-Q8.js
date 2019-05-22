7883510b47d9b1a4f0d564621c6390fb367a667d
AravinthSS <50924083+AravinthSS@users.noreply.github.com>
2019-05-22 22:57:25 +0530

    Update player-S1-Q8.js

--- player-S1-Q8.js
+++ player-S1-Q8.js
@@ -1,8 +1,11 @@
 let inputString = 'this is aravinth sankar from sivakasi';
+function toCamalCase(inputString)  {
 inputString = inputString.split(' ');
 inputString.forEach( (word, index) => {
   word = word[0].toUpperCase().concat(word.slice(1, word.length));
   inputString[index] = word;
 });
 inputString = inputString.join(' ')
-console.log(inputString);
+return inputString;
+}
+toCamalCase(inputString);
